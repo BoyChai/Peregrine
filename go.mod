23bb7dce6acc31efe74b1aeab5cbe40b7defd36d
BoyChai <1972567225@qq.com>
2024-09-25 14:46:44 +0800

    refactor: project renaming

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/BoyChai/Alerter
+module Peregrine
 
-go 1.19
+go 1.22.5
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Alerter")
+	fmt.Println("Hello Peregrine")
 }
